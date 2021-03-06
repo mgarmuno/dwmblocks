e4180a17707e0d94f439f9dacd4a61b9ef383f76
mgarmuno <miguelgarmm@gmail.com>
2021-03-06 17:25:00 +0100

    dwmblocks scripts

--- blocks.def.h
+++ blocks.def.h
@@ -2,8 +2,13 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-	{"Vol:", "sh /home/magneto/.config/mscripts/volume", 1 , 0},
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"Disk:", "/home/magneto/.local/bin/dwmblocks/dwmb-disk", 60, 0},
+	{"Bat:", "/home/magneto/.local/bin/dwmblocks/dwmb-battery2", 10, },
+	//{"Net:", "/home/magneto/.local/bin/dwmblocks/dwmb-internet", 0, 1},
+	{"Vol:", "/home/magneto/.local/bin/dwmblocks/dwmb-volume", 1, 0},
+	{"", "/home/magneto/.local/bin/dwmblocks/dwmb-date",  5, 0},
+	{"", "dwm -v", 360, 0},
+	{"   ", "",  5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
